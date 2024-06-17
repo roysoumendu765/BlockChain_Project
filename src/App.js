687241d14f72282adaf3533823430bcbc16407ea
Soumendu Roy <roysoumendu765@gmail.com>
2024-06-18 00:10:15 +0530

    added login component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Login from './components/Login/Login';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Login />
+      <Footer />
     </div>
   );
 }
