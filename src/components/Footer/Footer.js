ad565bd466e5be0670a4b97f7472d7f0a4561a24
Soumendu Roy <roysoumendu765@gmail.com>
2024-06-18 01:29:38 +0530

    updated footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-main">
+       <p className='footer-content'>
+         Made with <FavoriteIcon style={{fill: "red"}}/> Using Blockchain.
+       </p>
+    </div>
   )
 }
 
