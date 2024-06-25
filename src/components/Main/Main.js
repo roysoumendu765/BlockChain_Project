44155088048c7116629acf720b4bee80b8bae147
Akshay Bhandari <akshaybhandari0211@gmail.com>
2024-06-25 22:40:18 +0530

    Initial commit with main.js and main.css styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
-import FeedbackList from '../FeedbackList/FeedbackList';
-import Searchfeedback from '../Searchfeedback/Searchfeedback';
-import './Main.css';
+import React, { useState } from "react";
+import FeedbackList from "../FeedbackList/FeedbackList";
+import Searchfeedback from "../Searchfeedback/Searchfeedback";
+import "./Main.css";
 
 const Main = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   return (
-    <div className='mainpage-container'>
+    <div className="mainpage-container">
       Main Page
+      <h1 className="mainpage-title">Main Page</h1>
+      <div className="search-section">
+        <SearchFeedback setValue={setValue} />
+      </div>
+      <div className="feedback-section">
+        <FeedbackList value={value} />
+      </div>
       {/* <Searchfeedback setValue={setValue}/>
       <FeedbackList value={value}/> */}
     </div>
-  )
-}
+  );
+};
 
 export default Main;
